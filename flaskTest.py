0ba9fe047bcc673061184d2622ecb810b36d92a6
Akash Soni <akashsoni@Novomac9s-MacBook-Pro-2.local>
2018-08-14 21:34:26 +0530

    server started on consol

--- flaskTest.py
+++ flaskTest.py
@@ -1,31 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
 posts = [
-	{
-	'title':'apple',
-	'course':'swift 4',
-	'descreption':'this course gives you all that you need for development'
-	},
-	{
-	'title':'googel',
-	'course':'kotlin',
-	'descreption':'this course gives you all that you need for development'
-	}
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
-app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='About')
-
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
